2da17bf1b36d50962c73089a4b8f1f5655238d1f
lresco <luis.rebla@gmail.com>
2024-11-19 03:20:07 +0100

    Update logger.ts

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,36 @@
 import winston from "winston";
+import path from "path";
 
-const logger = winston.createLogger({
+interface StackInfo {
+  method: string;
+  relativePath: string;
+  line: string;
+  column: string;
+  file: string;
+  stack: string;
+}
+
+const customFormat = winston.format.printf((info) => {
+  if (info.level === "error") {
+    return `[${info.level}: ${info.timestamp}] ${info.message} ${info.stack}`;
+  }
+  return `[${info.level}: ${info.timestamp}] ${info.message}`;
+});
+
+export const logger = winston.createLogger({
+  level: "info",
   format: winston.format.combine(
     winston.format.timestamp({
       format: "YYYY-MM-DD hh:mm:ss",
     }),
-    winston.format.printf((info) => {
-      return `${info.timestamp}: ${info.message}`;
-    }),
+    winston.format.errors({ stack: true }),
+    customFormat,
   ),
   transports: [
-    new winston.transports.Console(),
     new winston.transports.File({ filename: "logs/combined.log" }),
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize()),
+    }),
   ],
 });
 
